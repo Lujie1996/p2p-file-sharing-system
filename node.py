f4e135ec2bc9e0a8ad368c66df35860e78ac8e49
Jie Lu <lujie1996@hotmail.com>
2019-05-30 18:09:26 -0700

    finger table has bug

--- node.py
+++ node.py
@@ -2,6 +2,7 @@ import sys
 import grpc
 import time
 import hashlib
+import threading
 from concurrent import futures
 import chord_service_pb2
 import chord_service_pb2_grpc
@@ -30,7 +31,7 @@ class Node(chord_service_pb2_grpc.ChordServicer):
         self.finger_table = []  # [(key, [successor_id, successor_address(ip:port)])]
         self.initial_id_addr_map = initial_id_addr_map
         self.logger = self.set_log()
-        self.only_main_thread = False
+        self.only_main_thread = True
         self.fix_fingure = FixFigure(self)
         self.stabilize = Stabilize(self)
         self.run()
@@ -289,7 +290,8 @@ class LocalChordCluster():
         node_identifiers = sorted(id_addr_map.keys())
 
         for i, node_id in enumerate(node_identifiers):
-            serve(id_addr_map[node_id], id_addr_map)
+            thread = threading.Thread(target=serve, args=(id_addr_map[node_id], id_addr_map))
+            thread.start()
 
 
 def serve(addr, id_addr_map):
